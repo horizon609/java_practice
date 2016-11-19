36a42ccca41a46d7ac25d3a7d66f72e7caa9cd0b
echoplex_x <horizon609@gmail.com>
2016-11-19 11:03:44 +0800

    add leetcode 231..

---
+++ src/leetcode/LeetCode231.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package leetcode;
+
+/**
+ * @author echoplex_x email:wangting-xy@360.cn
+ * @date 2016年11月19日
+ */
+public class LeetCode231 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
