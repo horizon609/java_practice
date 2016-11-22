6881de3df9d82fc9be7aab4dc9beac324b0a34f3
echoplex_x <horizon609@gmail.com>
2016-11-22 11:10:53 +0800

    add leetcode77..

---
+++ src/leetcode/Leetcode77.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author echoplex_x email:wangting-xy@360.cn
+ * @date 2016年11月22日
+ */
+public class Leetcode77 {
+    /**
+     * 最近已经碰到好几个这种dfs+回溯的套路了，只要理解了原理，这一类的问题就迎刃而解了。
+     */
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(combine(4, 2));
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        List<Integer> temp = new ArrayList<Integer>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        dfs(n,k,res,temp,1);
+        return res;
+    }
+
+    /**
+     * @param n
+     * @param k
+     * @param res
+     * @param temp
+     * @param i 
+     */
+    private static void dfs(int n, int k, List<List<Integer>> res, List<Integer> temp, int i) {
+        if(temp.size() == k){
+            res.add(new ArrayList<Integer>(temp));
+        }else{
+            for(int j = i;j<=n;j++){
+                if(k!=temp.size()){
+                    temp.add(j);
+                    dfs(n, k, res, temp, j+1);
+                    temp.remove(temp.size()-1);
+                }
+            }
+        }
+    }
+
+}
