d460f1f3816d2d52cf4db803088ad19c472762e4
echoplex_x <horizon609@gmail.com>
2016-12-30 18:08:46 +0800

    修改题设

--- src/leetcode/Leetcode410.java
+++ src/leetcode/Leetcode410.java
@@ -8,13 +8,11 @@ package leetcode;
  * @date 2016年11月11日
  */
 public class Leetcode410 {
-    /*
-     * Given an array which consists of non-negative integers and an integer m, 
-     * you can split the array into m non-empty continuous subarrays. 
-     * Write an algorithm to minimize the largest sum among these m subarrays.
-     * Input:
-     * nums = [7,2,5,10,8]
-     * m = 2
+    /**
+     * Input: nums = [7,2,5,10,8] m = 2 Output: 18 Explanation: There are four
+     * ways to split nums into two subarrays. The best way is to split it into
+     * [7,2,5] and [10,8], where the largest sum among the two subarrays is only
+     * 18.
      */
 
     /**
